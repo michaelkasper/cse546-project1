4e2aa065d1ccaa68ea332c5943be2ae2616fa860
Michael Kasper <mkasper1@godaddy.com>
2021-06-07 00:38:46 -0700

    added logic that creates many ec2 instance on each controller tick

--- Controller.js
+++ Controller.js
@@ -21,44 +21,46 @@ const ec2 = new AWS.EC2();
 
         if ( queueLength > 0 ) {
             const instanceReservations = await ec2.describeInstances( {} ).promise();
-            const instances            = instanceReservations.Reservations[ 0 ].Instances;
+            const instances            = instanceReservations.Reservations.map( reservation => reservation.Instances[ 0 ] );
 
+            const processorInstances = instances.filter( instance => instance.State.Name !== 'terminated' && !!instance.Tags.find( tag => tag.Key === 'processor' ) );
 
-            const processorInstances = instances.filter( instance => !!instance.Tags.find( tag => tag.Key === 'processor' ) );
-
-            if ( processorInstances.length < queueLength ) {
+            let count = processorInstances.length;
+            if ( count < queueLength ) {
                 //create ec2
-                try {
-                    const bootScript = await fs.readFile( 'scripts/processor.boot.sh', 'utf8' );
+                while ( count < queueLength && count < 20 ) {
+                    try {
+                        const bootScript = await fs.readFile( 'scripts/processor.boot.sh', 'utf8' );
 
-                    const result = await ec2.runInstances( {
-                        ImageId           : config.ami,
-                        InstanceType      : 't2.micro',
-                        IamInstanceProfile: {
-                            Arn: "arn:aws:iam::415900791134:instance-profile/CSE546-Webapp"
-                        },
-                        MinCount          : 1,
-                        MaxCount          : 1,
-                        UserData          : Buffer.from( bootScript ).toString( 'base64' ),
-                        KeyName           : config.keyName,
-                        SecurityGroupIds  : [ config.securityGroupId ]
-                    } ).promise();
+                        const result = await ec2.runInstances( {
+                            ImageId           : config.ami,
+                            InstanceType      : 't2.micro',
+                            IamInstanceProfile: {
+                                Arn: "arn:aws:iam::415900791134:instance-profile/CSE546-Webapp"
+                            },
+                            MinCount          : 1,
+                            MaxCount          : 1,
+                            UserData          : Buffer.from( bootScript ).toString( 'base64' ),
+                            KeyName           : config.keyName,
+                            SecurityGroupIds  : [ config.securityGroupId ]
+                        } ).promise();
 
-                    const newInstanceId = result.Instances[ 0 ].InstanceId;
+                        const newInstanceId = result.Instances[ 0 ].InstanceId;
 
-                    await ec2.createTags( {
-                        Resources: [ newInstanceId ], Tags: [
-                            {
-                                Key  : 'processor',
-                                Value: ''
-                            }
-                        ]
-                    } ).promise();
+                        await ec2.createTags( {
+                            Resources: [ newInstanceId ], Tags: [
+                                {
+                                    Key  : 'processor',
+                                    Value: ''
+                                }
+                            ]
+                        } ).promise();
 
-                } catch ( err ) {
-                    config.debug && console.log( 'create', err )
+                        count++;
+                    } catch ( err ) {
+                        config.debug && console.log( 'create', err )
+                    }
                 }
-
             }
         }
 
@@ -71,6 +73,6 @@ const ec2 = new AWS.EC2();
         // config.debug && console.log( pendingImages );
 
 
-        await new Promise( r => setTimeout( r, 100 ) );
+        await new Promise( r => setTimeout( r, 500 ) );
     }
 } )()
