5ff5c1b857224f7d43683ffa04145f1789181c54
Michael Kasper <mkasper1@godaddy.com>
2021-06-06 23:28:41 -0700

    bug fixing

--- Controller.js
+++ Controller.js
@@ -16,44 +16,59 @@ const ec2 = new AWS.EC2();
             AttributeNames: [ 'ApproximateNumberOfMessages' ]
         } ).promise();
 
+
         const queueLength = sqsAttributes.Attributes.ApproximateNumberOfMessages;
+
         if ( queueLength > 0 ) {
             const instanceReservations = await ec2.describeInstances( {} ).promise();
             const instances            = instanceReservations.Reservations[ 0 ].Instances;
 
+
             const processorInstances = instances.filter( instance => !!instance.Tags.find( tag => tag.Key === 'processor' ) );
 
-            if ( processorInstances < queueLength ) {
+            if ( processorInstances.length < queueLength ) {
                 //create ec2
-                const bootScript    = await fs.readFile( '/etc/hosts', 'utf8' );
-                const result        = await ec2.runInstances( {
-                    ImageId           : config.ami,
-                    InstanceType      : 't2.micro',
-                    IamInstanceProfile: "arn:aws:iam::415900791134:instance-profile/CSE546-Webapp",
-                    MinCount          : 1,
-                    MaxCount          : 1,
-                    UserData          : Buffer.from( bootScript ).toString( 'base64' )
-                } ).promise();
-                const newInstanceId = result.Instances[ 0 ].InstanceId;
+                try {
+                    const bootScript = await fs.readFile( 'scripts/processor.boot.sh', 'utf8' );
 
-                await ec2.createTags( {
-                    Resources: [ newInstanceId ], Tags: [
-                        {
-                            Key  : 'processor',
-                            Value: ''
-                        }
-                    ]
-                } ).promise();
+                    console.log( bootScript );
+
+                    const result = await ec2.runInstances( {
+                        ImageId           : config.ami,
+                        InstanceType      : 't2.micro',
+                        IamInstanceProfile: {
+                            Arn: "arn:aws:iam::415900791134:instance-profile/CSE546-Webapp"
+                        },
+                        MinCount          : 1,
+                        MaxCount          : 1,
+                        UserData          : Buffer.from( bootScript ).toString( 'base64' )
+                    } ).promise();
+
+                    const newInstanceId = result.Instances[ 0 ].InstanceId;
+
+                    await ec2.createTags( {
+                        Resources: [ newInstanceId ], Tags: [
+                            {
+                                Key  : 'processor',
+                                Value: ''
+                            }
+                        ]
+                    } ).promise();
+
+                } catch ( err ) {
+                    console.log( 'create', err )
+                }
+
             }
         }
 
 
-        const pendingImages = await s3.listObjectsV2( {
-            Bucket: config.s3Bucket,
-            Prefix: "pending/"
-        } ).promise();
-
-        console.log( pendingImages );
+        // const pendingImages = await s3.listObjectsV2( {
+        //     Bucket: config.s3Bucket,
+        //     Prefix: "pending/"
+        // } ).promise();
+        //
+        // console.log( pendingImages );
 
 
         await new Promise( r => setTimeout( r, 100 ) );
