d5ef4958b3e586242c8fb3e33964f762980b9909
Michael Kasper <mkasper1@godaddy.com>
2021-06-06 23:42:38 -0700

    bug fixes

--- Processor.js
+++ Processor.js
@@ -12,149 +12,14 @@ const ec2  = new AWS.EC2();
 const meta = new AWS.MetadataService();
 
 ( async () => {
-    const config     = await getConfig();
-    const data       = null;
-    // meta.request( "/latest/meta-data/instance-id", ( err, data ) => {
-    const instanceId = data || uuid.v4();
+    const config = await getConfig();
 
-    let timer = null;
+    meta.request( "/latest/meta-data/instance-id", ( err, data ) => {
+        const instanceId = data || uuid.v4();
 
-    const sqsConsumer = Consumer.create( {
-        sqs          : sqs,
-        queueUrl     : config.sqsInputUrl,
-        handleMessage: async ( message ) => {
-            const { s3key, requestId } = JSON.parse( message.Body );
+        let timer = null;
 
-            if ( timer ) {
-                clearTimeout( timer );
-            }
-
-            console.log( `PROCESSING: ${ requestId }` );
-
-
-            const ext       = s3key.split( '.' ).pop();
-            const localPath = join( process.cwd(), `${ instanceId }.${ ext }` );
-
-            let result = null;
-            let error  = false;
-
-            // flag so no other process can use
-            try {
-                const { TagSet: currentTags } = await s3.getObjectTagging( {
-                    Bucket: config.s3Bucket,
-                    Key   : s3key
-                } ).promise();
-
-                const currentFlag = currentTags.find( tag => tag.Key === 'instance' );
-                if ( currentFlag ) {
-                    // todo: validate instance is still active
-                    console.log( `SKIPPING: ${ requestId }` );
-                    return;
-                }
-
-                await s3.putObjectTagging( {
-                    Bucket : config.s3Bucket,
-                    Key    : s3key,
-                    Tagging: {
-                        TagSet: [
-                            {
-                                Key  : "processing",
-                                Value: instanceId
-                            },
-                            {
-                                Key  : "start",
-                                Value: Date.now().toString()
-                            }
-                        ]
-                    }
-                } ).promise();
-
-                const { TagSet: newTags } = await s3.getObjectTagging( {
-                    Bucket: config.s3Bucket,
-                    Key   : s3key
-                } ).promise();
-
-                const newFlag = newTags.find( tag => tag.Key === 'processing' );
-                if ( !newFlag || newFlag.Value !== instanceId ) {
-                    console.log( `SKIPPING: ${ requestId }` );
-                    return;
-                }
-
-            } catch ( err ) {
-                console.log( 'flag start', err );
-                return true;
-            }
-
-
-            // process image
-            try {
-                const s3File = await s3.getObject( {
-                    Bucket: config.s3Bucket,
-                    Key   : s3key
-                } ).promise();
-
-                await fs.writeFile( localPath, s3File.Body );
-
-                const { stdout } = await spawn( 'python3', [ 'image_classification.py', localPath ], {
-                    cwd    : config.SCRIPT_DIR,
-                    capture: [ 'stdout', 'stderr' ]
-                } );
-                result           = stdout.toString();
-
-            } catch ( err ) {
-                if ( err.stderr ) {
-                    //python error
-                    console.log( 'python', err.stderr );
-                }
-                console.log( 'process', err );
-                error = true;
-            }
-
-
-            // send message
-            try {
-                await sqs.sendMessage( {
-                    MessageBody           : JSON.stringify( { result, requestId, error } ),
-                    MessageDeduplicationId: requestId,
-                    MessageGroupId        : 'output',
-                    QueueUrl              : config.sqsOutputUrl
-                } ).promise()
-            } catch ( error ) {
-                console.log( 'message', error );
-            }
-
-
-            try {
-                await s3.putObjectTagging( {
-                    Bucket : config.s3Bucket,
-                    Key    : s3key,
-                    Tagging: {
-                        TagSet: [
-                            {
-                                Key  : "complete",
-                                Value: Date.now().toString()
-                            }
-                        ]
-                    }
-                } ).promise();
-            } catch ( error ) {
-                console.log( 'flag complete', error );
-            }
-
-            // cleanup
-            try {
-                await Promise.all( [
-                    fs.unlink( localPath ),
-                    s3.deleteObject( {
-                        Bucket: config.s3Bucket,
-                        Key   : s3key
-                    } ).promise()
-                ] );
-            } catch ( err ) {
-                console.log( 'cleanup', err );
-            }
-
-
+        const setTimer = () => {
             timer = setTimeout( async () => {
                 sqsConsumer.stop();
                 timer = null;
@@ -169,19 +34,163 @@ const meta = new AWS.MetadataService();
                     await new Promise( r => setTimeout( r, 2000 ) );
                 }
             }, 300000 );
+            console.log( `Timer set for 5min` );
+        }
 
-            return true;
-        },
+        const sqsConsumer = Consumer.create( {
+            sqs          : sqs,
+            queueUrl     : config.sqsInputUrl,
+            handleMessage: async ( message ) => {
+                const { s3key, requestId } = JSON.parse( message.Body );
+
+                if ( timer ) {
+                    clearTimeout( timer );
+                    console.log( `Clearing Timer` );
+                }
+
+                console.log( `PROCESSING: ${ requestId }` );
+
+
+                const ext       = s3key.split( '.' ).pop();
+                const localPath = join( process.cwd(), `${ instanceId }.${ ext }` );
+
+                let result = null;
+                let error  = false;
+
+                // flag so no other process can use
+                try {
+                    const { TagSet: currentTags } = await s3.getObjectTagging( {
+                        Bucket: config.s3Bucket,
+                        Key   : s3key
+                    } ).promise();
+
+                    const currentFlag = currentTags.find( tag => tag.Key === 'instance' );
+                    if ( currentFlag ) {
+                        // todo: validate instance is still active
+                        console.log( `SKIPPING: ${ requestId }` );
+                        return;
+                    }
+
+                    await s3.putObjectTagging( {
+                        Bucket : config.s3Bucket,
+                        Key    : s3key,
+                        Tagging: {
+                            TagSet: [
+                                {
+                                    Key  : "processing",
+                                    Value: instanceId
+                                },
+                                {
+                                    Key  : "start",
+                                    Value: Date.now().toString()
+                                }
+                            ]
+                        }
+                    } ).promise();
+
+                    const { TagSet: newTags } = await s3.getObjectTagging( {
+                        Bucket: config.s3Bucket,
+                        Key   : s3key
+                    } ).promise();
+
+                    const newFlag = newTags.find( tag => tag.Key === 'processing' );
+                    if ( !newFlag || newFlag.Value !== instanceId ) {
+                        console.log( `SKIPPING: ${ requestId }` );
+                        return;
+                    }
+
+                } catch ( err ) {
+                    console.log( 'flag start', err );
+                    return true;
+                }
+
+
+                // process image
+                try {
+                    const s3File = await s3.getObject( {
+                        Bucket: config.s3Bucket,
+                        Key   : s3key
+                    } ).promise();
+
+                    await fs.writeFile( localPath, s3File.Body );
+
+                    const { stdout } = await spawn( 'python3', [ 'image_classification.py', localPath ], {
+                        cwd    : config.scriptDir,
+                        capture: [ 'stdout', 'stderr' ]
+                    } );
+                    result           = stdout.toString();
+
+                } catch ( err ) {
+                    if ( err.stderr ) {
+                        //python error
+                        console.log( 'python', err.stderr );
+                    }
+                    console.log( 'process', err );
+                    error = true;
+                }
+
+
+                // send message
+                try {
+                    await sqs.sendMessage( {
+                        MessageBody           : JSON.stringify( { result, requestId, error } ),
+                        MessageDeduplicationId: requestId,
+                        MessageGroupId        : 'output',
+                        QueueUrl              : config.sqsOutputUrl
+                    } ).promise()
+                } catch ( error ) {
+                    console.log( 'message', error );
+                }
+
+
+                try {
+                    await s3.putObjectTagging( {
+                        Bucket : config.s3Bucket,
+                        Key    : s3key,
+                        Tagging: {
+                            TagSet: [
+                                {
+                                    Key  : "complete",
+                                    Value: Date.now().toString()
+                                }
+                            ]
+                        }
+                    } ).promise();
+                } catch ( error ) {
+                    console.log( 'flag complete', error );
+                }
+
+                // cleanup
+                try {
+                    await Promise.all( [
+                        fs.unlink( localPath ),
+                        s3.deleteObject( {
+                            Bucket: config.s3Bucket,
+                            Key   : s3key
+                        } ).promise()
+                    ] );
+                } catch ( err ) {
+                    console.log( 'cleanup', err );
+                }
+
+
+                setTimer();
+
+                return true;
+            },
+        } );
+
+        sqsConsumer.on( 'error', ( err ) => {
+            console.error( err.message );
+        } );
+
+        sqsConsumer.on( 'processing_error', ( err ) => {
+            console.error( err.message );
+        } );
+
+        setTimer();
+
+        sqsConsumer.start();
+
     } );
-
-    sqsConsumer.on( 'error', ( err ) => {
-        console.error( err.message );
-    } );
-
-    sqsConsumer.on( 'processing_error', ( err ) => {
-        console.error( err.message );
-    } );
-
-    sqsConsumer.start();
-    // } );
 } )();
