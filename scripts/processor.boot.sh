1eac5173f7d771488a53cc085942dff1a155952d
Michael Kasper <mkasper1@godaddy.com>
2021-06-07 09:52:52 -0700

    updating boot scripts

--- scripts/processor.boot.sh
+++ scripts/processor.boot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /var/www/app
-npm i
 nvm use
+npm i
 pm2 delete all
 pm2 start npm -- run start-processor
 pm2 save
--- scripts/webserver.boot.sh
+++ scripts/webserver.boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /var/www/app
+nvm use
 npm i
 pm2 delete all
 pm2 start npm -- run start
