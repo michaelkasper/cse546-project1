404c69fad5faf8a3d1da81d2b8d1837931ce9246
Michael Kasper <mkasper1@godaddy.com>
2021-06-07 23:00:35 -0700

    update to boot scripts

--- scripts/webserver.boot.sh
+++ scripts/webserver.boot.sh
@@ -7,7 +7,7 @@ runuser -u ubuntu -- git pull
 runuser -u ubuntu -- nvm use
 runuser -u ubuntu -- npm i
 runuser -u ubuntu -- pm2 delete all
-runuser -u ubuntu -- pm2 start "npm run webapp" --name "webapp" --interpreter=/home/ubuntu/.nvm/versions/node/v16.3.0/bin/node
-runuser -u ubuntu -- pm2 start "npm run controller" --name "controller" --interpreter=/home/ubuntu/.nvm/versions/node/v16.3.0/bin/node
+runuser -u ubuntu -- pm2 start "npm run webapp" --name "webapp"
+runuser -u ubuntu -- pm2 start "npm run controller" --name "controller"
 runuser -u ubuntu -- pm2 save
 runuser -u ubuntu -- pm2 resurrect
