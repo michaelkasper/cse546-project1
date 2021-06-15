763bf9bdd5ce0f159685101740f079eaf92757bc
Michael Kasper <mkasper1@godaddy.com>
2021-06-14 17:47:48 -0700

    updating boot scripts

--- scripts/processor.boot.sh
+++ scripts/processor.boot.sh
@@ -1,16 +1,30 @@
-#!/bin/bash
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
 
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
 su - ubuntu
-. ~/.profile
-source ~/.nvm/nvm.sh
 
 cd /var/www/app
 
 git reset --hard HEAD
 git pull
-nvm use
 npm i
-pm2 delete all
-pm2 start "npm run processor" --name "processor"
-pm2 save
-pm2 resurrect
+
+npm run processor
+--//
